82631a6def5452f0b95608f158b6f0f54df04222
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-07-09 20:44:54 +0200

    23.3 Mail v3

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -32,7 +32,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if(!mail.getToCC().equals("") && mail.getToCC()!=null) {
+        if(mail.getToCC()!=null && !mail.getToCC().equals("")  ) {
             mailMessage.setCc(mail.getToCC());
         }
         return mailMessage;
