70d564dca6847c6daa9aa1d86369c5a9cf0e659b
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-21 19:09:37 +0200

    Changed annotation to @RunWith(SpringRunner.class) and Autowired

--- src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
+++ src/test/java/com/crud/tasks/mapper/TaskMapperTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +16,11 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaskMapperTest {
 
-    @InjectMocks
+    @Autowired
     private TaskMapper taskMapper;
 
     @Test
