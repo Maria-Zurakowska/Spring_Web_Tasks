90a21477919cb6ad809a5f339ec647fb6280d291
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-30 10:06:45 +0200

    Added @NoArgsConstructor

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,10 +2,12 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class TaskDto {
     private Long id;
