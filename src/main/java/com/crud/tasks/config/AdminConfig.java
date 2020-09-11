a34432f39da661f8ed28e889a0a561607f43ebc6
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-09-11 13:09:22 +0200

    added fields for admin.name and company.details

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -10,4 +10,10 @@ public class AdminConfig {
     @Value("${admin.mail}")
     private String adminMail;
 
+    @Value("Kodilla User")
+    private String adminName;
+
+    @Value("ABC Sp. z o.o., ul. Marszałkowska 123, 01-000 Warszawa")
+    private String companyDetails;
+
 }
