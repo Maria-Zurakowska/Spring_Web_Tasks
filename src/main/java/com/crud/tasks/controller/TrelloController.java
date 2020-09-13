ee89c71a8b9760c54b0ab1537bbd5ce162697879
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-09-13 10:57:19 +0200

    change in request mappings (endpoints)

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -17,7 +17,7 @@ public class TrelloController {
     @Autowired
     private TrelloFacade trelloFacade;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getTrelloBoards")
+    @RequestMapping(method = RequestMethod.GET, value = "/boards")
     public List<TrelloBoardDto> getTrelloBoards() {
          return trelloFacade.fetchTrelloBoards();
 
@@ -27,8 +27,8 @@ public class TrelloController {
 //                .filter(b -> b.getName().equals("Kodilla"))
 //                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
     }
-        // POST request
-        @RequestMapping(method = RequestMethod.POST, value = "/createTrelloCard")
+
+        @RequestMapping(method = RequestMethod.POST, value = "/cards")
         public CreatedTrelloCardDto createTrelloCard(@RequestBody TrelloCardDto trelloCardDto){
         return trelloFacade.createCard(trelloCardDto);
     }
